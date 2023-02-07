11a4dc2e056242d7e162393d45cdf45a255ae540
Delliott <dlelliott100@outlook.com>
2023-02-07 03:52:31 +0000

    more user data

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import config
 import logging
 from typing import Union
 from pathlib import Path
+from math import floor, sqrt
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s (%(filename)s) - %(message)s')
 
@@ -10,6 +11,7 @@ try:
     from discord.ext import commands
     from discord import (
         __version__ as __discord__,
+        utils,
         Intents,
         Activity,
         ActivityType,
@@ -59,6 +61,10 @@ class GamBot(commands.Bot):
         return 0xffffff
 
     @staticmethod
+    def _time_now():
+        return floor(utils.utcnow().timestamp())
+
+    @staticmethod
     async def ephemeral_response(interaction: Interaction, reply: str, colour: int):
         await interaction.response.send_message(embed=Embed(colour=colour, description=reply), ephemeral=True)
 
@@ -67,24 +73,23 @@ class GamBot(commands.Bot):
             'create table if not exists user_data (id integer, money integer, gold integer, xp integer, pay_mult real, '
             'xp_mult real, daily_claimed integer, cons_dailies integer, blacklisted integer, premium integer)')
         await self.db.execute(
-            'create table if not exists inventories (user_id integer, money_pack_s integer, money_pack_m integer, '
+            'create table if not exists inventories (id integer, money_pack_s integer, money_pack_m integer, '
             'money_pack_l integer, gold_pack_s integer, gold_pack_m integer, gold_pack_l integer, jackpot_pack integer,'
-            ' payout_boost_c integer, payout_boost_r integer, payout_boost_e integer, xp_boost_c integer, xp_boost_r '
-            'integer, xp_boost_e integer)')
+            ' pay_boost_c integer, pay_boost_r integer, pay_boost_e integer, xp_boost_c integer, xp_boost_r integer, '
+            'xp_boost_e integer)')
         await self.db.execute(
-            'create table if not exists achievements (user_id integer, bj_max integer, bj_sevens integer, rou_mil '
-            'integer, rou_zero integer, poker_sf integer, poker_max integer, hl_max integer, hl_streak integer, '
-            'slot_jack integer, spin_jack integer, lott_win integer, scratch_win integer, million integer, billion '
-            'integer, legend integer)')
+            'create table if not exists achievements (id integer, bj_max integer, bj_sevens integer, rou_mil integer, '
+            'rou_zero integer, poker_sf integer, poker_max integer, hl_max integer, hl_str integer, slot_jack integer, '
+            'spin_jack integer, lott_win integer, scrat_win integer, mill integer, bill integer, legend integer)')
         await self.db.execute(
-            'create table if not exists active_boosts (user_id integer, booster_type text, payout_mult numeric, xp_mult'
-            ' numeric, lasts_until integer)')
+            'create table if not exists active_boosts (id integer, boost_type text, pay_mult real, xp_mult real, '
+            'lasts_until integer)')
         await self.db.execute(
             'create table if not exists daily_shop (item_1 text, item_2 text, item_3 text, item_4 text, item_5 text, '
             'item_6 text)')
         await self.db.commit()
 
-    async def user_data(self, user: User):
+    async def user_data(self, user: User) -> tuple:
         cursor = await self.db.execute('SELECT * FROM user_data WHERE id = ?', (user.id,))
         data = await cursor.fetchall()
         if not data:
@@ -94,31 +99,34 @@ class GamBot(commands.Bot):
             await self.db.commit()
         return data
 
-    async def money(self, user: User):
+    async def money(self, user: User) -> int:
         return (await self.user_data(user))[1]
 
-    async def gold(self, user: User):
+    async def gold(self, user: User) -> int:
         return (await self.user_data(user))[2]
 
-    async def xp(self, user: User):
+    async def xp(self, user: User) -> int:
         return (await self.user_data(user))[3]
 
-    async def pay_mult(self, user: User):
-        return (await self.user_data(user))[4]
+    async def rank(self, user: User) -> int:
+        return floor(sqrt((await self.xp(user)) / 8561) + 1)
 
-    async def xp_mult(self, user: User):
+    async def pay_mult(self, user: User) -> float:
+        return (await self.user_data(user))[4] + ((await self.rank(user)) - 1) / 50
+
+    async def xp_mult(self, user: User) -> float:
         return (await self.user_data(user))[5]
 
-    async def daily_claimed(self, user: User):
+    async def daily_claimed(self, user: User) -> bool:
         return True if (await self.user_data(user))[6] else False
 
-    async def cons_dailies(self, user: User):
+    async def cons_dailies(self, user: User) -> int:
         return (await self.user_data(user))[7]
 
-    async def is_blacklisted(self, user: User):
+    async def is_blacklisted(self, user: User) -> bool:
         return True if (await self.user_data(user))[8] else False
 
-    async def has_premium(self, user: User):
+    async def has_premium(self, user: User) -> bool:
         return True if (await self.user_data(user))[9] else False
 
     async def setup_hook(self) -> None:
